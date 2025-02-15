a0b64a50caf5a33c2f97a1c27d901834a0ec294e
Peter <petershotbox@gmail.com>
2025-02-15 22:47:05 +0000

    Added CORS back

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./routes/index.js";
-//import cors from "cors";
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Setting headers to link frontend and backend
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
