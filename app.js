7aeaab47bcecca94ec01aa6f15ebf79ed2b27772
Peter <petershotbox@gmail.com>
2025-02-15 22:50:56 +0000

    Back to start

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 app.use("/api", router);
 
 app.listen(port, () => {
