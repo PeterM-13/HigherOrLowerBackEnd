582edd559d5b90064744ad89e2ff3e08d8b64304
Peter <petershotbox@gmail.com>
2025-02-15 21:34:25 +0000

    Update to CORS

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ import morgan from "morgan";
 import router from "./routes/index.js";
 import cors from "cors";
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+
+// CORS configuration
+const corsOptions = {
+  origin: 'https://peter-metcalfe.co.uk/Leonardo/HigherOrLower/gamePage.html',
+  optionsSuccessStatus: 200
+};
 
 // Setting headers to link frontend and backend
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/api", router);
