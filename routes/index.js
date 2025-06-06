7b4e0466c93d50d3da39f3d1be627d68875fec2d
Peter <petershotbox@gmail.com>
2025-06-06 15:01:11 +0100

    Added more quesitons

--- routes/index.js
+++ routes/index.js
@@ -7,20 +7,26 @@ let players = [] // { name:player, score:0, voted:false }
 let currentRound = -1;
 
 let rounds = [
-  {prev:"Number of Leonardo UK sites (9)", current:"Funding MOD budgeted for Tempest for the next 10 years, in billions", ans:"lower"},
-  {prev:"Funding MOD budgeted for Tempest for the next 10 years, in billions (12)", current:"Number of sections on Leonardo parachute logo", ans:"higher"},
-  {prev:"Number of sections on Leonardo parachute logo (8)", current:"Percentage of women working in engineering in 2023 workforce", ans:"lower"},
-  {prev:"Percentage of women working in engineering in 2023 workforce (15.7)", current:"Leonardo’s 2024 revenues, in billions", ans:"lower"},
-  {prev:"Leonardo’s 2024 revenues, in billions (17.8)", current:"Number of Leonardo helicopter solutions currently in use", ans:"higher"},
-  {prev:"Number of Leonardo helicopter solutions currently in use (17)", current:"The number of years until GCAP will enter service", ans:"higher"},
-  {prev:"The number of years until GCAP will enter service (10)", current:"Overall percentage of apprentices in the workforce", ans:"higher"},
-  {prev:"Overall percentage of apprentices in the workforce (5.2)", current:"Number of airports with Leonardo air traffic control systems", ans:"lower"},
-  {prev:"Number of airports with Leonardo air traffic control systems (600)", current:"Number of apprentices recruited by Team Tempest Partners since 2018", ans:"lower"},
-  {prev:"Number of apprentices recruited by Team Tempest Partners since 2018 (1100)", current:"Leonardo’s investment into R&D, in 2024, in millions", ans:"lower"},
-  {prev:"Leonardo’s investment into R&D, in 2024, in millions (2500)", current:"How many people are working on Tempest across the UK", ans:"lower"},
-  {prev:"How many people are working on Tempest across the UK (3500)", current:"Number of countries using Leonardo radar systems", ans:"higher"},
-  {prev:"Number of countries using Leonardo radar systems (150)", current:"Number of years Leonardo’s heritage in EW spans", ans:"higher"},
-  {prev:"Number of years Leonardo’s heritage in EW spans (122)", current:"Number of Leonardo sites worldwide", ans:"lower"},
+  { prev: "Number of Leonardo UK sites (9)", current: "Funding MOD budgeted for Tempest for the next 10 years, in billions", ans: "lower" }, // 9 < 12
+  { prev: "Funding MOD budgeted for Tempest for the next 10 years, in billions (12)", current: "Number of sections on Leonardo parachute logo", ans: "higher" }, // 12 > 8
+  { prev: "Number of sections on Leonardo parachute logo (8)", current: "Percentage of women working in engineering in 2023 workforce", ans: "lower" }, // 8 < 15.7
+  { prev: "Percentage of women working in engineering in 2023 workforce (15.7)", current: "Leonardo’s 2024 revenues, in billions", ans: "lower" }, // 15.7 < 17.8
+  { prev: "Leonardo’s 2024 revenues, in billions (17.8)", current: "Number of Leonardo helicopter solutions currently in use", ans: "higher" }, // 17.8 > 17
+  { prev: "Number of Leonardo helicopter solutions currently in use (17)", current: "The number of years until GCAP will enter service", ans: "higher" }, // 17 > 10
+  { prev: "The number of years until GCAP will enter service (10)", current: "Overall percentage of apprentices in the workforce", ans: "higher" }, // 10 > 5.2
+  { prev: "Overall percentage of apprentices in the workforce (5.2)", current: "Number of British jobs supported by Leonardo, in thousands", ans: "lower" }, // 5.2 < 31.7
+  { prev: "Number of British jobs supported by Leonardo, in thousands (31.7)", current: "Number of airports with Leonardo air traffic control systems", ans: "lower" }, // 31.7 < 600
+  { prev: "Number of airports with Leonardo air traffic control systems (600)", current: "Number of apprentices recruited by Team Tempest Partners since 2018", ans: "lower" }, // 600 < 1100
+  { prev: "Number of apprentices recruited by Team Tempest Partners since 2018 (1100)", current: "Leonardo’s investment into R&D, in 2024, in millions", ans: "lower" }, // 1100 < 2500
+  { prev: "Leonardo’s investment into R&D, in 2024, in millions (2500)", current: "How many people are working on Tempest across the UK", ans: "lower" }, // 2500 < 3500
+  { prev: "How many people are working on Tempest across the UK (3500)", current: "Number of countries using Leonardo radar systems", ans: "higher" }, // 3500 > 150
+  { prev: "Number of countries using Leonardo radar systems (150)", current: "Number of years Leonardo’s heritage in EW spans", ans: "higher" }, // 150 > 122
+  { prev: "Number of years Leonardo’s heritage in EW spans (122)", current: "Number of Leonardo sites worldwide", ans: "lower" }, // 122 < 129
+  { prev: "Number of Leonardo sites worldwide (129)", current: "The exports Leonardo generates towards the UK economy annually, in millions", ans: "lower" }, // 129 < 930
+  { prev: "The exports Leonardo generates towards the UK economy annually, in millions (930)", current: "The Commute time from London to Luton by train, in minutes", ans: "higher" }, // 930 > 25
+  { prev: "The Commute time from London to Luton by train, in minutes (25)", current: "The number of Leonardo employees worldwide, in thousands", ans: "lower" }, // 25 < 53
+  { prev: "The number of Leonardo employees worldwide, in thousands (53)", current: "Percentage of Leonardo’s workforce made up of highly-skilled engineers", ans: "higher" }, // 53 > 50
+  { prev: "Percentage of Leonardo’s workforce made up of highly-skilled engineers (50)", current: "Number of Future Leaders offered an apprenticeship from our first cohort", ans: "higher" }, // 50 > 3
 ]
 
 // Get round
